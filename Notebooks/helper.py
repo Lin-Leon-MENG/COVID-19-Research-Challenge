4a7f245474febb74276db7180f7b8899f94ff40a
Lin Meng <lmeng24@hotmail.com>
2020-03-22 21:58:22 -0700

    parse JSON data

---
+++ Notebooks/helper.py
@@ -0,0 +1,17 @@
+class FileReader:
+    def __init__(self, file_path):
+        with open(file_path) as file:
+            content = json.load(file)
+            self.paper_id = content['paper_id']
+            self.abstract = []
+            self.body_text = []
+            # Abstract
+            for entry in content['abstract']:
+                self.abstract.append(entry['text'])
+            # Body text
+            for entry in content['body_text']:
+                self.body_text.append(entry['text'])
+            self.abstract = '\n'.join(self.abstract)
+            self.body_text = '\n'.join(self.body_text)
+    def __repr__(self):
+        return f'{self.paper_id}: {self.abstract[:200]}... {self.body_text[:200]}...'
